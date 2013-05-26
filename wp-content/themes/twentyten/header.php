4a94db1fa9ef707abd49dd8de108c8b2cfa93045
tedi3231 <tedi3231@gmail.com>
2013-05-26 21:35:19 +0800

    add template and modify index.php file

--- wp-content/themes/twentyten/header.php
+++ wp-content/themes/twentyten/header.php
@@ -12,7 +12,9 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<title><?php
+<title>
+<?php echo bloginfo("theme_url"); ?>
+<?php
 	/*
 	 * Print the <title> tag based on what is being viewed.
 	 */
@@ -34,7 +36,93 @@
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('template_url')?>/bootstrap/css/bootstrap.css"/>
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('template_url')?>/bootstrap/css/bootstrap-responsive.css"/>
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+  <script src="../assets/js/html5shiv.js"></script>
+<![endif]-->
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    <style type="text/css">
+      body {
+        padding-top: 1px;
+        padding-bottom: 40px;
+		background-color:gray;
+      }
+	  #header{
+	  	background-color:white;
+	}
+	#container{
+		background-color:white;
+	}
+	#header_menu{
+		padding-right:10px;
+	}
+	#header_menu a{
+		color:#5a5b5d;
+		font-size:1.1em;
+	}
+	#content{
+		background-color:white;
+		padding-left:75px;
+		padding-right:30px;
+		width:1065px;
+	}
+	#content .nav-list{
+		padding-left:6px;
+	}
+	#content .nav-list li>a{
+		padding-left:1px;
+		line-height:25px;
+		color:#7b7b7b;
+	}
+	#picture_news div.picture_title{
+		background-color:#B1CEE5;
+		margin-top:5px;
+		padding:5px;
+		text-align:center;
+	}
+	#picture_news_list{
+		margin-left:2px;
+	}
+	
+	#header_top p{
+		color:#4e8dab;
+		font-size:1.1em;
+	}
+
+	#header_top p>img{
+		padding-right:10px;
+	}
+	.news_content p{
+		color:#7b7b7b;
+	}
+	.news_title{
+		color:#80b7d6;
+		font-size:1.2em;
+		margin-top:20px;
+		padding-left:30px;
+	}
+	.nav{
+		margin-bottom:2px;
+	}
+	.nav li{
+		margin-left:10px;
+		margin-right:10px;
+	}
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+
+      @media (max-width: 980px) {
+        /* Enable use of floated navbar text */
+        .navbar-text.pull-right {
+          float: none;
+          padding-left: 5px;
+          padding-right: 5px;
+        }
+	}	  
+</style>
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php
 	/* We add some JavaScript to pages with the comment form
@@ -53,56 +141,34 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="wrapper" class="hfeed">
+<div id="container" class="background-color:white;">
 	<div id="header">
-		<div id="masthead">
-			<div id="branding" role="banner">
-				<?php $heading_tag = ( is_home() || is_front_page() ) ? 'h1' : 'div'; ?>
-				<<?php echo $heading_tag; ?> id="site-title">
-					<span>
-						<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-					</span>
-				</<?php echo $heading_tag; ?>>
-				<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-
-				<?php
-					// Compatibility with versions of WordPress prior to 3.4.
-					if ( function_exists( 'get_custom_header' ) ) {
-						// We need to figure out what the minimum width should be for our featured image.
-						// This result would be the suggested width if the theme were to implement flexible widths.
-						$header_image_width = get_theme_support( 'custom-header', 'width' );
-					} else {
-						$header_image_width = HEADER_IMAGE_WIDTH;
-					}
-
-					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
-					if ( is_singular() && current_theme_supports( 'post-thumbnails' ) &&
-							has_post_thumbnail( $post->ID ) &&
-							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' ) ) &&
-							$image[1] >= $header_image_width ) :
-						// Houston, we have a new header image!
-						echo get_the_post_thumbnail( $post->ID );
-					elseif ( get_header_image() ) :
-						// Compatibility with versions of WordPress prior to 3.4.
-						if ( function_exists( 'get_custom_header' ) ) {
-							$header_image_width  = get_custom_header()->width;
-							$header_image_height = get_custom_header()->height;
-						} else {
-							$header_image_width  = HEADER_IMAGE_WIDTH;
-							$header_image_height = HEADER_IMAGE_HEIGHT;
-						}
-					?>
-						<img src="<?php header_image(); ?>" width="<?php echo $header_image_width; ?>" height="<?php echo $header_image_height; ?>" alt="" />
-					<?php endif; ?>
-			</div><!-- #branding -->
-
-			<div id="access" role="navigation">
-			  <?php /* Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff */ ?>
-				<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentyten' ); ?>"><?php _e( 'Skip to content', 'twentyten' ); ?></a></div>
-				<?php /* Our navigation menu. If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assiged to the primary position is the one used. If none is assigned, the menu with the lowest ID is used. */ ?>
-				<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
-			</div><!-- #access -->
-		</div><!-- #masthead -->
+		<div id="header">
+				<div id="header_top" class="row-fluid" style="height:90px;background-color:white;">
+					<div class="span6" style="padding-left:20px;padding-top:10px;">
+						<img src="img/header.png" height="80px" width="340px"/>
+					</div>	
+					<div class="span6 text-right">
+						<div class="row-fluid" style="padding-top:40px;">
+							<div class="span9 text-right">
+								<p class="text-info large"><img src="img/employee_road.png"/>员工通道</p>
+							</div>
+							<div class="span3 text-left">
+								<p class="text-info"><img src="img/email.png"/>企业邮箱</p>
+							</div>
+						</div>
+					</div>	
+				</div>
+				<div id="header_menu" class="row-fluid">
+					<div class="span4"></div>
+					<div class="span8 text-right" style="padding-right:30px;background-color:#eeeeee;">
+						<ul class="nav nav-pills">
+							<li><a href="#">首页</a></li>
+							<li><a href="#">关于我们</a></li>
+							<li><a href="#">新闻中心</a></li>
+						</ul>
+					</div>
+				</div>
 	</div><!-- #header -->
 
 	<div id="main">
--- wp-content/themes/twentyten/index.php
+++ wp-content/themes/twentyten/index.php
@@ -1,32 +1,227 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
- */
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Bootstrap, from Twitter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-get_header(); ?>
+    <!-- Le styles -->
+	<!-- <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />-->
+	<link href="<?php bloginfo('template_url')?>/bootstrap/css/bootstrap.css" rel="stylesheet">
+	<link href="<?php bloginfo('template_url') ?>/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="<?php bloginfo('template_url') ?>/site.css" rel="stylesheet">
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+  </head>
 
-		<div id="container">
-			<div id="content" role="main">
+  <body>
+	 <div class="container" style="background-color:white;">
+		 <div id="header">
+				<div id="header_top" class="row-fluid" style="height:90px;background-color:white;">
+					<div class="span6" style="padding-left:20px;padding-top:10px;">
+					<img src="<?php bloginfo('template_url') ?>/bootstrap/img/header.png" height="80px" width="340px"/>
+					</div>	
+					<div class="span6 text-right">
+						<div class="row-fluid" style="padding-top:40px;">
+							<div class="span9 text-right">
+								<p class="text-info large"><img src="<?php bloginfo('template_url') ?>/bootstrap/img/employee_road.png"/>员工通道</p>
+							</div>
+							<div class="span3 text-left">
+							<p class="text-info"><img src="<?php bloginfo('template_url') ?>/bootstrap/img/email.png"/>企业邮箱</p>
+							</div>
+						</div>
+					</div>	
+				</div>
+				<div id="header_menu" class="row-fluid">
+					<div class="span4"></div>
+					<div class="span8 text-right" style="padding-right:30px;background-color:#eeeeee;">
+						<?php wp_nav_menu(array('menu_class'=>'nav nav-pills','theme_location'=>'primary')) ?>
+						<!--<ul class="nav nav-pills">
+							<li><a href="#">首页</a></li>
+							<li><a href="#">关于我们</a></li>
+							<li><a href="#">新闻中心</a></li>
+							<li><a href="#">企业人文</a></li>
+							<li><a href="#">荣誉成就</a></li>
+							<li><a href="#">党建园地</a></li>
+							<li><a href="#">联系我们</a></li>
+						</ul>-->
+					</div>
+				</div>
+				<div id="header_slider" style="margin-top:0px;">
+				<img src="<?php bloginfo('template_url')?>/bootstrap/img/slider1.png"/>
+				</div>
+			</div>
+			<div>
+				<div id="content" class="row-fluid">
+					<div class="span9" style="padding-right:15px;">
+						<!-- complex news start-->
+						<div class="row">
+							<div class="news_title">
+								<div class="row">
+									<div class="span10">
+										<span >综合新闻</span>
+									</div>
+									<div class="span2 text-right"><img src="<?php bloginfo('template_url') ?>/bootstrap/img/more.png"/></div>
+								</div>
+							</div>
+							<hr style="margin:2px;"/>
+							<ul class="nav nav-list">
+								<?php
+									$args = array('numberposts'=>7,'offset'=>0	,'category'=>5);
+									$myposts = get_posts($args);
+									foreach( $myposts as $post):setup_postdata($post);
+									?>
+									<li>
+										<a href="<?php the_permalink();?>">
+											<?php echo mysql2date('[ Y/m/d ]',$post->post_date);?>
+											<?php echo the_title('','',FALSE)?>
+										</a>
+									</li>
+								<?php endforeach;?>
+							</ul>
+						</div>
+						<!-- complex news end -->
 
-			<?php
-			/* Run the loop to output the posts.
-			 * If you want to overload this in a child theme then include a file
-			 * called loop-index.php and that will be used instead.
-			 */
-			 get_template_part( 'loop', 'index' );
-			?>
-			</div><!-- #content -->
-		</div><!-- #container -->
+						<!-- picture news start-->
+						<div id="picture_news" class="row">
+							<div class="news_title">
+								<div class="row">
+									<div class="span10">
+										<span>图片新闻</span>
+									</div>
+									<div class="span2 text-right"><img src="<?php bloginfo('template_url') ?>/bootstrap/img/more.png"/></div>
+								</div>
+							</div>
+							<hr style="margin:2px;"/>
+							<div id="picture_news_list" class="row" style="padding-top:10px;">
+								<div class="span4">
+								<div><img src="<?php bloginfo('template_url') ?>/bootstrap/img/news_1.png"/></div>
+									<div class="picture_title">多米尼克总统视察上海建工承建即将竣工的国宾馆项目</div>
+								</div>
+								<div class="span4">
+								<div><img src="<?php bloginfo('template_url') ?>/bootstrap/img/news_2.png"/></div>
+									<div class="picture_title">多米尼克总统视察上海建工承建即将竣工的国宾馆项目</div>
+								</div>
+								<div class="span4">
+								<div><img src="<?php bloginfo('template_url') ?>/bootstrap/img/news_3.png"/></div>
+									<div class="picture_title">多米尼克总统视察上海建工承建即将竣工的国宾馆项目</div>
+								</div>
+							</div>	
+						</div>
+						<!-- picture news end -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+						<!-- dangjian and employee start -->
+						<div id="dang_and_employee" class="row" style="margin-left:-35px;">
+							<div class="span8">
+								<div class="news_title">
+									<div class="row">
+										<div class="span10">
+											<span>党建园地</span>
+										</div>
+										<div class="span2 text-right"><img src="<?php bloginfo('template_url') ?>/bootstrap/img/more.png"/></div>
+									</div>
+								</div>
+								<hr style="margin:2px;"/>
+								<ul class="nav nav-list">
+									<li><a>上海建工积极扩展南昌建筑市场名师带出高徒加快培育高科技人才</a></li>
+									<li><a>上海建工积极扩展南昌建筑市场名师带出高徒加快培育高科技人才</a></li>
+									<li><a>上海建工积极扩展南昌建筑市场名师带出高徒加快培育高科技人才</a></li>
+									<li><a>上海建工积极扩展南昌建筑市场名师带出高徒加快培育高科技人才</a></li>
+									<li><a>上海建工积极扩展南昌建筑市场名师带出高徒加快培育高科技人才</a></li>
+									<li><a>上海建工积极扩展南昌建筑市场名师带出高徒加快培育高科技人才</a></li>
+									<li><a>上海建工积极扩展南昌建筑市场名师带出高徒加快培育高科技人才</a></li>
+								</ul>
+							</div>
+							<div class="span4">
+								<div class="news_title">
+									<div class="row">
+										<div class="span7">
+											<span>员工风采</span>
+										</div>
+										<div class="span5 text-right"><img src="<?php bloginfo('template_url')?>/bootstrap/img/more.png"/></div>
+									</div>
+								</div>
+								<hr style="margin:2px;"/>
+								<div style="margin-top:10px;">
+									<div><img src="img/news_3.png"/></div>
+									<div class="text-center">多米尼克总统视察上海建工承建即将竣工的国宾馆项目</div>
+								</div>
+							</div>
+
+						</div>
+						<!-- dangjian and employee end -->
+
+					</div>
+					<div class="span3">
+						<div id="project" class="row" style="background-color:#eeeeee;padding-left:10px;padding-right:10px;">
+								<div class="news_title">
+									<div class="row">
+										<div class="span7">
+											<span>经典项目</span>
+										</div>
+										<div class="span5 text-right"><img src="img/more.png"/></div>
+									</div>
+								</div>
+								<div style="margin-top:10px;">
+									<div><img class="img-polaroid" src="img/news_3.png"/></div>
+									<div class="text-center">多米尼克总统视察上海上海建工积极扩展南昌建筑市场名师带出高徒加快培育高科技人才上海建工积极扩展南昌建筑市场名师带出高徒加快培育高科技人才上海建工积极扩展南昌建筑市场名师带建工承建即将竣工的国宾馆项目</div>
+								</div>
+						</div>
+						<br/>
+						<div id="zhaoping" class="row">
+							<img src="img/news_2.png"/>
+						</div>
+						<br/>
+						<div id="project" class="row"> 
+								<div class="news_title">
+									<div class="row">
+										<div class="span12">
+											<span>友情链接</span>
+										</div>
+									</div>
+								</div>
+								<hr style="margin:2px;"/>
+								<ul class="nav nav-list">
+									<li><a href="#">www.wedoapp.com</a></li>
+									<li><a href="#">www.wedoapp.com</a></li>
+									<li><a href="#">www.wedoapp.com</a></li>
+									<li><a href="#">www.wedoapp.com</a></li>
+									<li><a href="#">www.wedoapp.com</a></li>
+								</ul>
+						</div>
+					</div>
+				</div>
+		</div>		
+		<hr style="margin-left:10px;margin-right:10px;"/>
+		<div id="footer" class="row">
+			 <div class="span4 text-right">
+				 <img src="img/gongshang.png"/>
+			 </div>
+			 <div class="span1 text-left">
+				 <img src="img/banner.png" />
+			 </div>
+			 <div class="span7">
+				 <div class="row">
+					 <div class="span12 text-left" style="margin-top:5px;">
+					 公司简介|证券信息|知识产权|法律声明|联系我们
+					 </div>
+				 </div>
+				 <div class="row">
+					 <div class="span12 text-left" style="margin-top:5px;">
+						 Copyright@上海建工集团股份有限公司 All rights reserved 沪ICP备:05055280
+					 </div>
+				 </div>
+			 </div>
+	   </div>
+	  </div>
+        <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+<script src="<?php bloginfo('template_url')?>/bootstrap/js/bootstrap.js"></script>
+  </body>
+</html>
+
